700a58417e7e5895ed2a25765157de8c597a3667
Yann E. MORIN <yann.morin.1998@free.fr>
2012-12-16 06:56:13 +0000

    package/slirp: new package
    Signed-off-by: "Yann E. MORIN" <yann.morin.1998@free.fr>
    Cc: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>

--- package/slirp/slirp.mk
+++ package/slirp/slirp.mk
@@ -1,17 +1,21 @@
-################################################################################
+#############################################################
 #
-# slirp
+# Spice-slirp
 #
-################################################################################
+#############################################################
 
-SLIRP_VERSION = 4.6.1
-SLIRP_SOURCE = libslirp-$(SLIRP_VERSION).tar.xz
-SLIRP_SITE = https://gitlab.freedesktop.org/slirp/libslirp/uploads/83b199ea6fcdfc0c243dfde8546ee4c9
-SLIRP_LICENSE = BSD-3-Clause
-SLIRP_LICENSE_FILES = COPYRIGHT
-SLIRP_CPE_ID_VENDOR = libslirp_project
-SLIRP_CPE_ID_PRODUCT = libslirp
+# There's no tarball releases of slirp, so we use the git repo
+# Also, there's no tag, so we use a random SHA1 (master's HEAD
+# of today)
+SLIRP_VERSION         = 8c2da74c1385242f20799fec8c04f8378edc6550
+SLIRP_SITE            = git://anongit.freedesktop.org/spice/slirp
+SLIRP_LICENSE         = BSD-4c BSD-2c
+# Note: The license file 'COPYRIGHT' is missing from the sources,
+# although some files refer to it.
 SLIRP_INSTALL_STAGING = YES
-SLIRP_DEPENDENCIES = libglib2
 
-$(eval $(meson-package))
+# As we're using the git tree, there's no ./configure,
+# so we need to autoreconf.
+SLIRP_AUTORECONF      = YES
+
+$(eval $(autotools-package))
