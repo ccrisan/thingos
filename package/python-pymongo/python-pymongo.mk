270bc85730099e4f39a66138d16f9f69fa73e43b
Calin Crisan <ccrisan@gmail.com>
2020-11-23 11:23:15 +0200

    python-pymongo: Update to 3.11.1

--- package/python-pymongo/python-pymongo.mk
+++ package/python-pymongo/python-pymongo.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYMONGO_VERSION = 3.8.0
+PYTHON_PYMONGO_VERSION = 3.11.1
 PYTHON_PYMONGO_SOURCE = pymongo-$(PYTHON_PYMONGO_VERSION).tar.gz
-PYTHON_PYMONGO_SITE = https://files.pythonhosted.org/packages/19/77/da358f5729ff046ceaf6c6a86755f9d8285719f93df6da15bb2440367d7e
+PYTHON_PYMONGO_SITE = https://files.pythonhosted.org/packages/15/dc/bc9f2692cd9ece34236950db1c27573ab28a2a5d1d651af57c67839aa593
 PYTHON_PYMONGO_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
