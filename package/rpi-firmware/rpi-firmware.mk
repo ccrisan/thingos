8884b48257570a3e5d58cdb239e63ee5690e4625
Calin Crisan <ccrisan@gmail.com>
2021-12-13 20:30:32 +0200

    rpi-firmware: Update to 4877a7d918be6fab22ce5d345feefd9c43200a90

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 2ef3800b1b71c0e30c2a61ecad6e007ae96ea342
+RPI_FIRMWARE_VERSION = 4877a7d918be6fab22ce5d345feefd9c43200a90
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
