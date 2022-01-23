386ddd6639548170e7aec6e1301512298a3f68d6
Calin Crisan <ccrisan@gmail.com>
2022-01-23 23:16:59 +0200

    raspberrypi(all): Fix fiwmare file name copy

--- board/raspberrypi2/postscript.sh
+++ board/raspberrypi2/postscript.sh
@@ -9,10 +9,10 @@ cp ${BOARD_DIR}/cmdline.txt ${BOOT_DIR}
 cp ${BOARD_DIR}/fwupdater.gz ${BOOT_DIR}
 cp ${BOARD_DIR}/initrd.gz ${BOOT_DIR}
 cp ${IMG_DIR}/zImage ${BOOT_DIR}/kernel.img
-cp ${IMG_DIR}/bcm2709-rpi-2-b.dtb ${BOOT_DIR}
+cp ${IMG_DIR}/*.dtb ${BOOT_DIR}
 cp ${RPI_FW_DIR}/bootcode.bin ${BOOT_DIR}
-cp ${RPI_FW_DIR}/start.elf ${BOOT_DIR}
-cp ${RPI_FW_DIR}/fixup.dat ${BOOT_DIR}
+cp ${RPI_FW_DIR}/start*.elf ${BOOT_DIR}
+cp ${RPI_FW_DIR}/fixup*.dat ${BOOT_DIR}
 
 # copy overlays
 mkdir -p ${BOOT_DIR}/overlays
--- board/raspberrypi4/postscript.sh
+++ board/raspberrypi4/postscript.sh
@@ -8,10 +8,9 @@ cp ${BOARD_DIR}/config.txt ${BOOT_DIR}
 cp ${BOARD_DIR}/cmdline.txt ${BOOT_DIR}
 cp ${BOARD_DIR}/initrd.gz ${BOOT_DIR}
 cp ${IMG_DIR}/zImage ${BOOT_DIR}/kernel.img
-cp ${IMG_DIR}/bcm2711-rpi-4-b.dtb ${BOOT_DIR}
-cp ${IMG_DIR}/bcm2711-rpi-cm4.dtb ${BOOT_DIR}
-cp ${RPI_FW_DIR}/start.elf ${BOOT_DIR}
-cp ${RPI_FW_DIR}/fixup.dat ${BOOT_DIR}
+cp ${IMG_DIR}/*.dtb ${BOOT_DIR}
+cp ${RPI_FW_DIR}/start*.elf ${BOOT_DIR}
+cp ${RPI_FW_DIR}/fixup*.dat ${BOOT_DIR}
 
 # copy overlays
 mkdir -p ${BOOT_DIR}/overlays
