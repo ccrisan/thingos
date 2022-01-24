0fa4da381ed1db112bd0a4375cfb61c3e1448dfb
Calin Crisan <ccrisan@gmail.com>
2022-01-24 16:03:14 +0200

    Remove currently unsupported boards

--- board/nanopineo2/boot.cmd
+++
@@ -1,6 +0,0 @@
-setenv bootargs console=ttyS0,115200 earlyprintk root=/dev/mmcblk0p2 rootwait ro no_console_suspend panic=10 quiet loglevel=1
-
-fatload mmc 0 0x46000000 Image
-fatload mmc 0 0x48000000 sun50i-h5-nanopi-neo2.dtb
-
-booti 0x46000000 - 0x48000000
