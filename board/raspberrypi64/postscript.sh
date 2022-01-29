ac8c488ee9da1198393f2181ad27bf35aaa51389
Calin Crisan <ccrisan@gmail.com>
2022-01-29 21:04:25 +0200

    raspberrypi64: Use GZ compressed kernel image

--- board/raspberrypi64/postscript.sh
+++ board/raspberrypi64/postscript.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 
-set -e
-
 RPI_FW_DIR=${TARGET}/../images/rpi-firmware
+
+mkdir -p ${BOOT_DIR}/overlays
 
 cp ${BOARD_DIR}/config.txt ${BOOT_DIR}
 cp ${BOARD_DIR}/cmdline.txt ${BOOT_DIR}
 cp ${BOARD_DIR}/initrd.gz ${BOOT_DIR}
-gzip -c ${IMG_DIR}/Image > ${BOOT_DIR}/kernel.img
+cp ${IMG_DIR}/Image.gz ${BOOT_DIR}/kernel.gz
 cp ${IMG_DIR}/*.dtb ${BOOT_DIR}
 cp ${RPI_FW_DIR}/start*.elf ${BOOT_DIR}
 cp ${RPI_FW_DIR}/fixup*.dat ${BOOT_DIR}
-
-# copy overlays
-mkdir -p ${BOOT_DIR}/overlays
 cp ${RPI_FW_DIR}/overlays/*.dtbo ${BOOT_DIR}/overlays
