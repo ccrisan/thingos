e322bce8087014cbbf68ed763f0cae770ee73aff
Calin Crisan <ccrisan@gmail.com>
2022-02-27 17:59:24 +0200

    raspberrypi64: Add missing bootcode.bin

--- board/raspberrypi64/postscript.sh
+++ board/raspberrypi64/postscript.sh
@@ -7,6 +7,7 @@ cp ${BOARD_DIR}/cmdline.txt ${BOOT_DIR}
 cp ${BOARD_DIR}/initrd.gz ${BOOT_DIR}
 cp ${BINARIES_DIR}/Image.gz ${BOOT_DIR}
 cp ${BINARIES_DIR}/*.dtb ${BOOT_DIR}
+cp ${BINARIES_DIR}/rpi-firmware/bootcode.bin ${BOOT_DIR}
 cp ${BINARIES_DIR}/rpi-firmware/start*.elf ${BOOT_DIR}
 cp ${BINARIES_DIR}/rpi-firmware/fixup*.dat ${BOOT_DIR}
 cp ${BINARIES_DIR}/rpi-firmware/overlays/*.dtbo ${BOOT_DIR}/overlays
