a5765d487cab3d682f3aec848b78e96475f66542
Calin Crisan <ccrisan@gmail.com>
2022-04-24 19:49:57 +0300

    rpi-userland: Update to c4fd1b8986c6d6d4ae5cd51e65a8bbeb495dfa4e

--- package/rpi-userland/rpi-userland.mk
+++ package/rpi-userland/rpi-userland.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_USERLAND_VERSION = 6e8f786db223c2ab6eb9098a5cb0e5e1b25281cd
+RPI_USERLAND_VERSION = c4fd1b8986c6d6d4ae5cd51e65a8bbeb495dfa4e
 RPI_USERLAND_SITE = $(call github,raspberrypi,userland,$(RPI_USERLAND_VERSION))
 RPI_USERLAND_LICENSE = BSD-3-Clause
 RPI_USERLAND_LICENSE_FILES = LICENCE
