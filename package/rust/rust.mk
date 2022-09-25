084415c6e43a6b015f03b45b1a9fdfc9e2c06cf3
James Hilliard <james.hilliard1@gmail.com>
2022-09-24 23:45:25 -0600

    package/{rust, rust-bin}: bump to version 1.64.0
    Link to Rust 1.64.0 annoucement: https://blog.rust-lang.org/2022/09/22/Rust-1.64.0.html
    
    Newest version of the source archives have been retrieved with their hash values,
    and the signature of the .asc files have been verified as follows:
    $ curl -fsSL https://static.rust-lang.org/rust-key.gpg.ascii | gpg --import
    $ gpg --verify <filename.asc> <filename>
    
    There is no typographical error in the packages according to the check-pakage utility:
    $ ./utils/check-package package/rust-bin/*
    $ ./utils/check-package package/rust/*
    
    The testsuite tool were successfully run for rust and rust-bin packages to test
    the Rust toolchain under 1.64.0:
    $ ./support/testing/run-tests -k -d dl/ -o testsuite tests.package.test_rust.TestRustBin
    $ ./support/testing/run-tests -k -d dl/ -o testsuite tests.package.test_rust.TestRust
    
    In order to verify the compatibility of packages depending on Rust 1.64.0,
    tests using `./utils/test-pkg` were run.
    You may want to execute the test-pkg command after creating a `.config` file
    enabling the corresponding BR2_PACKAGE, for example:
    Create a file `buildroot/ripgrep.config` containing "BR2_PACKAGE_RIPGREP=y"
    Then execute:
    $ ./utils/test-pkg -d test-pkg -c ripgrep.config -p ripgrep
    
    Results:
    librsvg OK
    ripgrep OK
    suricata OK
    bat OK
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/rust/rust.mk
+++ package/rust/rust.mk
@@ -4,11 +4,7 @@
 #
 ################################################################################
 
-<<<<<<< HEAD
-RUST_VERSION = 1.58.1
-=======
-RUST_VERSION = 1.62.0
->>>>>>> 6ea42ff9cf (package/{rust, rust-bin}: bump to version 1.62.0)
+RUST_VERSION = 1.64.0
 RUST_SOURCE = rustc-$(RUST_VERSION)-src.tar.xz
 RUST_SITE = https://static.rust-lang.org/dist
 RUST_LICENSE = Apache-2.0 or MIT
@@ -18,17 +14,11 @@ HOST_RUST_PROVIDES = host-rustc
 
 HOST_RUST_DEPENDENCIES = \
 	toolchain \
+	host-pkgconf \
+	host-python3 \
 	host-rust-bin \
 	host-openssl \
 	$(BR2_CMAKE_HOST_DEPENDENCY)
-
-ifeq ($(BR2_PACKAGE_PYTHON3),y)
-HOST_RUST_PYTHON_VERSION = $(PYTHON3_VERSION_MAJOR)
-HOST_RUST_DEPENDENCIES += host-python3
-else
-HOST_RUST_PYTHON_VERSION = $(PYTHON_VERSION_MAJOR)
-HOST_RUST_DEPENDENCIES += host-python
-endif
 
 HOST_RUST_VERBOSITY = $(if $(VERBOSE),2,0)
 
@@ -54,7 +44,7 @@ define HOST_RUST_CONFIGURE_CMDS
 		echo 'target = ["$(RUSTC_TARGET_NAME)"]'; \
 		echo 'cargo = "$(HOST_RUST_BIN_DIR)/cargo/bin/cargo"'; \
 		echo 'rustc = "$(HOST_RUST_BIN_DIR)/rustc/bin/rustc"'; \
-		echo 'python = "$(HOST_DIR)/bin/python$(HOST_RUST_PYTHON_VERSION)"'; \
+		echo 'python = "$(HOST_DIR)/bin/python$(PYTHON3_VERSION_MAJOR)"'; \
 		echo 'submodules = false'; \
 		echo 'vendor = true'; \
 		echo 'extended = true'; \
@@ -67,6 +57,7 @@ define HOST_RUST_CONFIGURE_CMDS
 		echo 'sysconfdir = "$(HOST_DIR)/etc"'; \
 		echo '[rust]'; \
 		echo 'channel = "stable"'; \
+		echo 'musl-root = "$(STAGING_DIR)"' ; \
 		echo '[target.$(RUSTC_TARGET_NAME)]'; \
 		echo 'cc = "$(TARGET_CROSS)gcc"'; \
 		echo '[llvm]'; \
@@ -75,12 +66,29 @@ define HOST_RUST_CONFIGURE_CMDS
 endef
 
 define HOST_RUST_BUILD_CMDS
-	cd $(@D); $(HOST_MAKE_ENV) $(HOST_DIR)/bin/python$(HOST_RUST_PYTHON_VERSION) x.py build
+	cd $(@D); $(HOST_MAKE_ENV) $(HOST_DIR)/bin/python$(PYTHON3_VERSION_MAJOR) x.py build
 endef
 
+HOST_RUST_INSTALL_OPTS = \
+	--prefix=$(HOST_DIR) \
+	--disable-ldconfig
+
+define HOST_RUST_INSTALL_RUSTC
+	cd $(@D)/build/tmp/tarball/rust/$(RUSTC_HOST_NAME)/rust-$(RUST_VERSION)-$(RUSTC_HOST_NAME); \
+		./install.sh $(HOST_RUST_INSTALL_OPTS) --components=rustc,cargo,rust-std-$(RUSTC_HOST_NAME)
+endef
+
+ifeq ($(BR2_PACKAGE_HOST_RUSTC_TARGET_ARCH_SUPPORTS),y)
+define HOST_RUST_INSTALL_LIBSTD_TARGET
+	cd $(@D)/build/tmp/tarball/rust-std/$(RUSTC_TARGET_NAME)/rust-std-$(RUST_VERSION)-$(RUSTC_TARGET_NAME); \
+		./install.sh $(HOST_RUST_INSTALL_OPTS)
+endef
+endif
+
 define HOST_RUST_INSTALL_CMDS
-	cd $(@D); $(HOST_MAKE_ENV) $(HOST_DIR)/bin/python$(HOST_RUST_PYTHON_VERSION) x.py dist
-	cd $(@D); $(HOST_MAKE_ENV) $(HOST_DIR)/bin/python$(HOST_RUST_PYTHON_VERSION) x.py install
+	cd $(@D); $(HOST_MAKE_ENV) $(HOST_DIR)/bin/python$(PYTHON3_VERSION_MAJOR) x.py dist
+	$(HOST_RUST_INSTALL_RUSTC)
+	$(HOST_RUST_INSTALL_LIBSTD_TARGET)
 endef
 
 $(eval $(host-generic-package))
