fe288bae714c4af250f8888845fc9260378a4b63
Calin Crisan <ccrisan@gmail.com>
2022-10-17 14:57:32 +0300

    python-bleak: Bump version to 0.19.0

--- package/python-bleak/python-bleak.mk
+++ package/python-bleak/python-bleak.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_BLEAK_VERSION = 0.12.1
+PYTHON_BLEAK_VERSION = 0.19.0
 PYTHON_BLEAK_SOURCE = bleak-$(PYTHON_BLEAK_VERSION).tar.gz
-PYTHON_BLEAK_SITE = https://files.pythonhosted.org/packages/67/4b/ba00c69a24a8d08c86f02caa45727a2ad2844c7bee9630ccb063be226f71
+PYTHON_BLEAK_SITE = https://files.pythonhosted.org/packages/19/39/ce32196148fd57f3eccffd9d246e78956855abe3b0c5038cf6166c75b7d8
 PYTHON_BLEAK_SETUP_TYPE = setuptools
 PYTHON_BLEAK_LICENSE = MIT
 PYTHON_BLEAK_LICENSE_FILES = LICENSE
