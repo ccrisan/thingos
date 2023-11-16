afd7f797c47490619e51c114f51c8b5c0e71caab
Calin Crisan <ccrisan@gmail.com>
2023-11-16 20:07:03 +0200

    nanopir1: Use pre-built u-boot

--- board/nanopir1/postscript.sh
+++ board/nanopir1/postscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cp ${BINARIES_DIR}/boot.scr ${BOOT_DIR}
-cp ${BINARIES_DIR}/sun8i-h3-nanopi-r1.dtb ${BOOT_DIR}
+cp ${BOARD_DIR}/boot.scr ${BOOT_DIR}
+cp ${BOARD_DIR}/u-boot-sunxi-with-spl.bin ${BINARIES_DIR}
 
 mkdir -p ${BOOT_DIR}/overlays
 cp ${BUILD_DIR}/linux-custom/arch/arm/boot/dts/overlays/*.dtbo ${BOOT_DIR}/overlays
