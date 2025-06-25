06ade56513367a06434070053d4fa1a75d0e61c1
Calin Crisan <ccrisan@gmail.com>
2025-06-25 18:52:11 +0300

    build-docker.sh: Run with invoked UID:GID

--- build-docker.sh
+++ build-docker.sh
@@ -13,5 +13,8 @@ cd $(dirname "$0")
 printenv | grep -E ^THINGOS > ${ENV_FILE}
 
 args="${@}"
-docker run --privileged -it --rm -v "$(pwd)":/os -e TB_CUSTOM_CMD="./build.sh ${args}" --env-file ${ENV_FILE} "${DOCKER_BUILDER_IMAGE}" 
-
+docker run --privileged -it --rm -u $(id -u):$(id -g) \
+       -v "$(pwd)":/os \
+       -e TB_CUSTOM_CMD="./build.sh ${args}" \
+       --env-file ${ENV_FILE} \
+       "${DOCKER_BUILDER_IMAGE}"
