8c85ab898742150d74924eb2c1ba143af7435140
Calin Crisan <ccrisan@gmail.com>
2025-06-25 18:26:56 +0300

    Add build-docker.sh helper script

---
+++ build-docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DOCKER_BUILDER_IMAGE="ccrisan/thingos-builder"
+ENV_FILE=$(mktemp)
+
+function cleanup() {
+    rm -f ${ENV_FILE}
+}
+
+trap cleanup EXIT
+
+cd $(dirname "$0")
+printenv | grep -E ^THINGOS > ${ENV_FILE}
+
+args="${@}"
+docker run --privileged -it --rm -v "$(pwd)":/os -e TB_CUSTOM_CMD="./build.sh ${args}" --env-file ${ENV_FILE} "${DOCKER_BUILDER_IMAGE}" 
+
